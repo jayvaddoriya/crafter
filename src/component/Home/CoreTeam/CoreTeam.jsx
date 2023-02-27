65c036fd51a95878ff53c6fa8cd0321e7a4ec123
jay vaddoriya <jayvaddoriya06@gmail.com>
2023-02-28 00:31:05 +0530

    gif mobile issues fix

--- src/component/Home/CoreTeam/CoreTeam.jsx
+++ src/component/Home/CoreTeam/CoreTeam.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./coreTeam.scss";
 import coreTeamMobileWeb from "../../../assets/gifs/Web mockup.gif";
-import TeamMobileWeb from "../../../assets/gifs/Mobile Mockup.gif";
+import TeamMobileWeb from "../../../assets/gifs/Mobile GIF.gif";
 import Button from "../../Button/Button";
 import { Col, Row } from "antd";
 
